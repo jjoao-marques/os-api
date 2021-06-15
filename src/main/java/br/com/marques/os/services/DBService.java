13a80f81f4acbbde13fcc5e647485a299a99b1b5
João Marques <joaomarques.jm991@gmail.com>
2021-06-15 01:56:24 -0300

    Ajuste DBService

--- src/main/java/br/com/marques/os/services/DBService.java
+++ src/main/java/br/com/marques/os/services/DBService.java
@@ -26,8 +26,8 @@ public class DBService {
     private OSRepository osRepository;
 
     public void InstanceDB() {
-        Technician t1 = new Technician(null, "João Marques", "49469111893", "16997990177");
-        Client c1 = new Client(null, "Taiane Santos", "54168773833", "16997251587");
+        Technician t1 = new Technician(null, "João Marques", "46314857015", "16997990155");
+        Client c1 = new Client(null, "Taiane Santos", "69581639071", "16997251544");
 
         OS os1 = new OS();
         os1.setPriority(Priority.HIGH);
